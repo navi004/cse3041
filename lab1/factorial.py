8e397e12da4c1641bb35c7ba6e8b4b1e1f11394d
NaveenNidadavolu <115283282+navi004@users.noreply.github.com>
2023-07-27 16:05:55 +0530

    Update factorial.py

--- lab1/factorial.py
+++ lab1/factorial.py
@@ -1,5 +1,5 @@
 #Factorial
-a = int(input("Enter the number))
+a = int(input("Enter the number : "))
 result  = 1 
 while(1):
     result = a * result
@@ -7,3 +7,18 @@ while(1):
     if(a<=0):
         print(result)
         break
+
+#With for loop
+num = int(input("Enter a number: "))
+
+factorial = 1
+
+# check if the number is negative, positive or zero
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print("The factorial of",num,"is",factorial)
