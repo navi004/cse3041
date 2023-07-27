b6fb4c242c82e501c3798998d8923bdb1a1e9e23
NaveenNidadavolu <115283282+navi004@users.noreply.github.com>
2023-07-27 16:35:54 +0530

    Update gpa.py

--- lab1/gpa.py
+++ lab1/gpa.py
@@ -4,3 +4,26 @@
     <= 7 cgpa < 8 - good
     <= 6 cgpa < 5 - better
     cgpa < 5 - poor"""
+def cgpa_segregation():
+    n = int(input("Enter the number of students: "))
+    i = 1  
+    while i in range(1, n):
+        cgpa = float(input("Enter the CGPA: "))
+        if cgpa > 10 or cgpa < 0:
+            print("Invalid CGPA")
+        else:
+            if 9 <= cgpa <= 10:
+                print("Outstanding")
+            elif 8 <= cgpa < 9:
+                print("Excellent")
+            elif 7 <= cgpa < 8:
+                print("Good")
+            elif 6 <= cgpa < 7:
+                print("Better")
+            elif 5 <= cgpa < 6:
+                print("Below Average")
+            elif cgpa < 5:
+                print("Poor")
+        i += 1
+
+cgpa_segregation()
