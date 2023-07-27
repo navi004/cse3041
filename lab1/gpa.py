8f61e6747afc8a27b38929bd8d8b3113a25520f9
NaveenNidadavolu <115283282+navi004@users.noreply.github.com>
2023-07-27 16:11:17 +0530

    Create gpa.py

---
+++ lab1/gpa.py
@@ -0,0 +1,6 @@
+#write a program to segregate student based on cgpa .the details are as follows :
+""" <=9 cgpa <= 10 - outstanding
+    <=8 cgpa < 9 - excellent
+    <= 7 cgpa < 8 - good
+    <= 6 cgpa < 5 - better
+    cgpa < 5 - poor"""
