12858df88d3f75bda67f23d689d567a0e1881a40
NaveenNidadavolu <115283282+navi004@users.noreply.github.com>
2023-07-27 15:58:30 +0530

    Create ifelse.py

---
+++ lab1/ifelse.py
@@ -0,0 +1,14 @@
+#Probelem-2
+#If Statement
+"""mark1 = int(input("Enter Mark1 : "))
+mark2 = int(input("Enter Mark2 :"))
+
+if(mark1 >= 90 and mark2 >= 90):
+    print("Excellent")
+elif(mark1 >= 90 or mark2 >= 90):
+    if(mark1 < 50 or mark2 < 50):
+        print("Need to take special classes")
+    else:
+    print("Good")
+elif(mark1 < 90 and mark2 < 90):
+    print("Need to improve")"""
