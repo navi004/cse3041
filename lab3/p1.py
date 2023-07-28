eb72247b5cc939190ee168162433b5bd32babf08
NaveenNidadavolu <115283282+navi004@users.noreply.github.com>
2023-07-28 12:15:07 +0530

    Update p1.py

--- lab3/p1.py
+++ lab3/p1.py
@@ -11,3 +11,18 @@ while(i<=n):
         i = i+1
 average = sum/n
 print("Average of class is",average)
+
+
+n = int(input("Enter the number of students : "))
+i=1
+sum = 0
+while(i<=n):
+    mark = int(input("Enter the mark of student  : "))
+    if(mark < 0):
+        print("Invalid input")
+        break
+    sum = sum + mark
+    i = i+1
+else:
+    average = sum/n
+    print("Average of class is",average)
