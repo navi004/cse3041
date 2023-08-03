38c0c97e6029854e66e6484e7f8ab2670fdd22ac
NaveenNidadavolu <115283282+navi004@users.noreply.github.com>
2023-08-03 22:00:52 +0530

    regular expression redno validation

--- lab5/p2.py
+++ lab5/p2.py
@@ -3,6 +3,14 @@
 """
 <2Xnumber><3Xalpha><4Xnumber>
 """
+"""
+^:               Asserts the start of the string.
+[1-9]:           Matches any digit from 1 to 9 (ensuring the registration number doesn't start with 0).
+[0-9]:           Matches any digit from 0 to 9.
+[a-zA-Z]{3}:     Matches exactly three alphabetic characters (both uppercase and lowercase letters are allowed).
+[0-9]{4}:        Matches exactly four digits.
+$:               Asserts the end of the string.
+"""
 import re
 pattern = r"^[1-9][0-9][a-zA-Z]{3}[0-9]{4}$"
 regNo = input("Enter the RegdNo :")
