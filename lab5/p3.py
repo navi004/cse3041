794f04d69041d8a3768871787662983125f098a3
NaveenNidadavolu <115283282+navi004@users.noreply.github.com>
2023-08-03 23:01:17 +0530

    Ceaser Cipher

--- lab5/p3.py
+++ lab5/p3.py
@@ -1,4 +1,8 @@
 #Problem 1 Ceaser cipher
+"""The Caesar cipher is a simple substitution cipher, 
+where each letter in the plaintext is shifted a fixed number of positions down the alphabet. 
+In this case, the key for the shift is set to 3.
+"""
 
 s = input("Enter message: ")
 key = 3
