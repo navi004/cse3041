1afe8a1639901fc79d4ffd0193682c0e463329f9
NaveenNidadavolu <115283282+navi004@users.noreply.github.com>
2023-08-08 12:18:29 +0530

    Create p1.py

---
+++ lab6/p1.py
@@ -0,0 +1,13 @@
+#List is a type of mutable sequence in python
+#Contains mutliple values that are logically related
+#indexing,sliding,adding,multiplication
+
+lst = []
+N = int(input("Enter N : "))
+
+for i in range(N):
+        val = int(input("Enter the number : "))
+        lst.append(val)
+print(lst)
+
+
