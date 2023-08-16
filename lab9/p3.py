b227ce868bf30bf44ed612de66b0c0853f230281
NaveenNidadavolu <115283282+navi004@users.noreply.github.com>
2023-08-16 15:23:44 +0530

    Update p3.py

--- lab9/p3.py
+++ lab9/p3.py
@@ -29,3 +29,8 @@ def func3(x1,y1,x2,y2,x3,y3):
 func3(1,4,2,6,6,8)
 
 
+def func3(x1,y1,x2,y2,x3,y3):
+    area = abs((1/2)*(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2)))
+    print(area)
+
+func3(1,4,2,6,6,8)
