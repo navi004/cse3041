c152ce3440f7e7f36748b65924d8f8682a06cc3c
NaveenNidadavolu <115283282+navi004@users.noreply.github.com>
2023-08-16 15:22:59 +0530

    Update p2.py

--- lab9/p2.py
+++ lab9/p2.py
@@ -1,3 +1,15 @@
+#LEGB
+
+#Global Scope
+x = 99
+
+def func(y):
+    #x = 79
+    z = x + y
+    return z
+
+print(func(1))
+
 #Factorial
 
 def factorial(n):
@@ -10,3 +22,26 @@ def factorial(n):
 
 print(factorial(5))
     
+#GCD
+def gcd(m,n,i=1):
+
+    i=min(m,n)
+    if(m%i==0 and n%i==0):
+        return i
+    else:
+        i = i-1
+        return gcd(m,n,i)
+
+print(gcd(100,60))
+
+
+
+
+
+def hcf(a,b):
+    if(b==0):
+        return(a)
+    else:
+        return hcf(b,a%b)
+
+print(hcf(60,100))
