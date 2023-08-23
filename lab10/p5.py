b225e2aecf682b5a07aead0a4afb2f28adfd9597
NaveenNidadavolu <115283282+navi004@users.noreply.github.com>
2023-08-23 14:31:07 +0530

    Update p5.py

--- lab10/p5.py
+++ lab10/p5.py
@@ -1,11 +1,14 @@
 
 
-# taking total sum
+# reading content from csv file and calculating the total marks
 fp = open("marks.csv","r+")
+fw = open("empty.txt","w+")
 fp.seek(0)
 l = []
 line = fp.readline()
 heading = line
+heading = heading.replace("\n","")
+fw.write(heading+",Total\n")
 while True:
     line = fp.readline()
     if not line:
@@ -13,6 +16,6 @@ while True:
     line = line.replace("\n","")
     st = line.split(",")
     totalsum = int(st[1]) + int(st[2]) + int(st[3])
-    st.append(totalsum)
-    l.append(st)
+    fw.write(line+","+str(totalsum)+"\n")
 fp.close()
+fw.close()
