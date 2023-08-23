3a35696207968315dda1a288b129921d3f9184c5
NaveenNidadavolu <115283282+navi004@users.noreply.github.com>
2023-08-23 15:37:26 +0530

    Create p4.py

---
+++ lab11/p4.py
@@ -0,0 +1,28 @@
+#Student class with name and age as attributes.then create a list of students
+# and display their names in increasing order of age
+class student:
+    counter = 0
+    def __init__(self,a,n):
+        self.full_name = n
+        self.age = a
+        student.counter = student.counter + 1  
+
+
+s = []
+m = int(input("Enter the number of students :"))
+for i in range(0,m):
+    name = input("Enter the name :")
+    age = int(input("Enter the age :"))
+    stud = student(name,age)
+    s.append(stud)
+print(s)
+for i in range(0,m):
+    print("Name",s[i].full_name," Age:",s[i].age)
+
+for i in range(0,m):
+    for j in range(len(s)):
+        if s[i].age > s[j].age:
+            s[j],s[i] = s[i],s[j]
+    print("After Sorting")
+    for i in range(0,m):
+        print("Name",s[i].full_name," Age:",s[i].age)
